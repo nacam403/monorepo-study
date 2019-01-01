dd3602a6af41058e9649d0dce68c3277d952aa48
nacam403 <satnakam@gmail.com>
2019-01-01 23:22:08 +0900

    TypeScriptでExpressアプリを書く土台作り。

--- packages/my-server/src/index.ts
+++ packages/my-server/src/index.ts
@@ -1,3 +1,13 @@
+import express from 'express'
 import { hello } from 'my-common'
 
-hello()
+const app = express()
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  hello()
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
