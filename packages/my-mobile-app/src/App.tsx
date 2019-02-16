dc1ecdc99d9a819f465dad19bf0f5204c05efc37
nacam403 <satnakam@gmail.com>
2019-02-16 20:15:41 +0900

    StaciNavigatorを使う。

--- packages/my-mobile-app/src/App.tsx
+++ packages/my-mobile-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-const App = () => <Home />
+const App = () => <AppContainer />
 
 export default App
 
@@ -12,6 +13,21 @@ const Home = () => (
   </View>
 )
 
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      title: 'ヘッダー',
+    },
+  },
+)
+
+const AppContainer = createAppContainer(AppNavigator)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,6 +36,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   text: {
-    fontSize: 20,
+    fontSize: 25,
   },
 })
