a45f4889224176eb17521747e7aa22542e7ed1da
nacam403 <satnakam@gmail.com>
2019-07-12 16:35:21 +0900

    addon-knobsの練習

--- packages/my-web-app/.storybook/config.js
+++ packages/my-web-app/.storybook/config.js
@@ -1,8 +1,11 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
 
 function loadStories() {
   const req = require.context('../src', true, /\.story\.tsx?$/)
   req.keys().forEach(story => req(story))
 }
+
+addDecorator(withKnobs)
 
 configure(loadStories, module)
--- packages/my-web-app/src/Sample.story.tsx
+++ packages/my-web-app/src/Sample.story.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 // @ts-ignore
 import { Button } from '@storybook/react/demo'
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button onClick={action('clicked.')}>Hello Button</Button>
+    <Button onClick={action('clicked.')}>
+      {text('Label', 'Hello Storybook')}
+    </Button>
   ))
   .add('with emoji', () => (
     <Button onClick={action('clicked.')}>
