fa408336d5e579ff9ee82788de723093a5900c67
nacam403 <satnakam@gmail.com>
2019-08-12 22:36:22 +0900

    見つかったペリフェラルを画面に表示する。

--- packages/ble-mobile-app/src/App.tsx
+++ packages/ble-mobile-app/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, SafeAreaView, View, Text, Button } from 'react-native'
 
 import { scan } from './BleManagerWrapper'
 
+const blankPeripheral = {
+  id: '-',
+  name: '-',
+}
+
 const App = () => {
+  const [discoveredPeripheral, setDiscoveredPeripheral] = useState(
+    blankPeripheral,
+  )
+
   return (
     <SafeAreaView style={style.safeAreaView}>
-      <Button onPress={scan} title="scan" />
+      <Button
+        onPress={async () => {
+          const discoveredPeripheral = await scan()
+          setDiscoveredPeripheral(discoveredPeripheral || blankPeripheral)
+        }}
+        title="scan"
+      />
+
+      <View>
+        <Text>{`Discovered Peripheral ID: ${discoveredPeripheral.id}`}</Text>
+        <Text>{`Discovered Peripheral Name: ${
+          discoveredPeripheral.name
+        }`}</Text>
+      </View>
     </SafeAreaView>
   )
 }
